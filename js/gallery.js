161597a7c162e747162b10c19535952542780dbb
Gavin64DD <50460376+Gavin64DD@users.noreply.github.com>
2021-05-07 10:32:24 -0400

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,14 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              window.oRequestAnimationFrame      ||
-              window.msRequestAnimationFrame     ||
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame       ||
+					window.webkitRequestAnimationFrame ||
+					window.mozRequestAnimationFrame    ||
+					window.oRequestAnimationFrame      ||
+					window.msRequestAnimationFrame     ||
+					function(/* function */ callback, /* DOMElement */ element){
+						window.setTimeout(callback, 1000 / 60);
+					};
+})();
 
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
@@ -18,45 +18,45 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+requestAnimFrame( animate );
+var currentTime = new Date().getTime();
+if (mLastFrameTime === 0) {
+mLastFrameTime = currentTime;
+}
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+if ((currentTime - mLastFrameTime) > mWaitTime) {
+swapPhoto();
+mLastFrameTime = currentTime;
+}
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
 
-  if(mCurrentIndex >= mImages.length)
-  {
-    mCurrentIndex = 0;
-  }
+if(mCurrentIndex >= mImages.length)
+{
+mCurrentIndex = 0;
+}
 
-  if(mCurrentIndex < 0) {
-    mCurrentIndex = mImages.length-1;
-  }
+if(mCurrentIndex < 0) {
+mCurrentIndex = mImages.length-1;
+}
 
-  document.getElementById('photo').src = mImages[mCurrentIndex].img;
+document.getElementById('photo').src = mImages[mCurrentIndex].img;
 
-  var location = document.getElementsByClassName('location')[0];
-  location.innerHTML = "Location: " + mImages[mCurrentIndex].location;
+var location = document.getElementsByClassName('location')[0];
+location.innerHTML = "Location: " + mImages[mCurrentIndex].location;
 
-  var description = document.getElementsByClassName('description')[0];
-  description.innerHTML = "Description: " + mImages[mCurrentIndex].description;
+var description = document.getElementsByClassName('description')[0];
+description.innerHTML = "Description: " + mImages[mCurrentIndex].description;
 
-  var date = document.getElementsByClassName('date')[0];
-  date.innerHTML = "Date: " + mImages[mCurrentIndex].date;
+var date = document.getElementsByClassName('date')[0];
+date.innerHTML = "Date: " + mImages[mCurrentIndex].date;
 
-  mLastFrameTime = 0;
-  mCurrentIndex += 1;
-	console.log('swap photo');
+mLastFrameTime = 0;
+mCurrentIndex += 1;
+console.log('swap photo');
 }
 
 var mCurrentIndex = 0;
@@ -72,52 +72,68 @@ var mJson;
 var mUrl = 'images.json';
 
 function fetchJSON() {
-  mRequest.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-         mJson = JSON.parse(mRequest.responseText);
-         iterateJSON();
-      }
-  };
-  mRequest.open("GET", mUrl, true);
-  mRequest.send();
+mRequest.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		 mJson = JSON.parse(mRequest.responseText);
+		 iterateJSON();
+	}
+};
+mRequest.open("GET", mUrl, true);
+mRequest.send();
 }
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
+return function(e) {
+galleryImage.img = e.target;
+mImages.push(galleryImage);
+}
 }
 
 $(document).ready( function() {
-  fetchJSON();
-	// This initially hides the photos' metadata information
-	// $('.details').eq(0).hide();
+fetchJSON();
+// This initially hides the photos' metadata information
+// $('.details').eq(0).hide();
 
 });
 
 window.addEventListener('load', function() {
 
-	console.log('window loaded');
+console.log('window loaded');
 
 }, false);
 
 function iterateJSON() {
-  for(x = 0; x < mJson.images.length; x++)
-  {
-    mImages[x] = new GalleryImage();
-    mImages[x].location = mJson.images[x].imgLocation;
-    mImages[x].description = mJson.images[x].description;
-    mImages[x].date = mJson.images[x].date;
-    mImages[x].img = mJson.images[x].imgPath;
-  }
+for(x = 0; x < mJson.images.length; x++)
+{
+mImages[x] = new GalleryImage();
+mImages[x].location = mJson.images[x].imgLocation;
+mImages[x].description = mJson.images[x].description;
+mImages[x].date = mJson.images[x].date;
+mImages[x].img = mJson.images[x].imgPath;
+}
 }
 
 function GalleryImage() {
-	var location;
-	var description;
-	var date;
-  var img;
+var location;
+var description;
+var date;
+var img;
+};
+
+function rotate(){
+	if ($(".moreIndicator").hasClass("rot90")) {
+			$(".moreIndicator").removeClass("rot90").addClass("rot270");
+			$(".details").slideToggle( "slow" );
+		} else {
+			$(".moreIndicator").removeClass("rot270").addClass("rot90");
+			$(".details").slideToggle( "slow" );
+		}
+};
+
+div #nav{
+#prevPhoto.click();
+
+#nextPhoto.click();
 }
